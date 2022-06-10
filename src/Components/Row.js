39f557c6d58ef9d9298bde701f2645746c033412
gituser1 <poosis15@gmail.com>
2022-06-11 02:34:04 +0530

    initial commit

---
+++ src/Components/Row.js
@@ -0,0 +1,32 @@
+import React,{useState, useEffect} from 'react'
+import axios from '../axios';
+
+const baseUrl="https://image.tmdb.org/t/p/original";
+
+function Row({title, fetchUrl}) {
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        };
+        fetchData();
+    },[fetchUrl] ); // eslint-disable-line react-hooks/exhaustive-deps
+    console.table(movies);
+    return (
+        <div className="row">
+            <h1> {title} </h1>
+            <div className="row_posters">
+                {movies.map(movie => (
+                    <img src={`${baseUrl}${movie.poster_path}`} alt={movie.name} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Row
